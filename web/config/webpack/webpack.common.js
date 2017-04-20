d91e4cae9e316de6e2f9da66d28243d7a17b80fc
Francisco Javier Sevilla <Pacheta@localhost.localdomain>
2017-04-21 00:30:39 +0200

    required all scripts

--- web/config/webpack/webpack.common.js
+++ web/config/webpack/webpack.common.js
@@ -1,8 +1,41 @@
+const webpack = require('webpack');
+const helpers = require('../helpers');
+
+/*
+ * Webpack Plugins
+ */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const AOT = helpers.hasNpmFlag('aot');
+
+
 module.exports = function (options) {
     isProd = options.env === 'production';
     return {
-
-
+        entry: {
+            'main': AOT ? './src/main.aot.ts' : './src/main.ts',
+            'polyfills': './src/polyfills.ts'
+        },
+        resolve: {
+            alias: {
+                'shared': helpers.root('src', 'app', 'shared'),
+            },
+            extensions: ['.ts', '.js', '.json'],
+            modules: [helpers.root('src'), helpers.root('node_modules')],
+        },
+        node: {
+            global: true,
+            crypto: 'empty',
+            process: true,
+            module: false,
+            clearImmediate: false,
+            setImmediate: false
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'src/index.html',
+                chunksSortMode: 'dependency'
+            }),
+        ]
     }
-
 }
--- web/config/webpack/webpack.dev.js
+++ web/config/webpack/webpack.dev.js
@@ -1,12 +1,22 @@
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js'); // the settings that are common to prod and dev
 
+const helpers = require('../helpers');
+
 const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
 
 module.exports = function (options) {
   return webpackMerge(commonConfig({
     env: ENV
   }), {
-
-  });
+      devtool: '#inline-source-map',
+      output: {
+        path: helpers.root('target', 'site', 'web'),
+        filename: '[name].js',
+        sourceMapFilename: '[name].map',
+        chunkFilename: '[id].chunk.js',
+        library: 'ac_[name]',
+        libraryTarget: 'var',
+      }
+    });
 };
